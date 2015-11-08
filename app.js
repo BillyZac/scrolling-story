2305329182f5bea6a3b48bdb560d98659be5c459
Zachary Smith <billyzacsmith@gmail.com>
2015-11-08 14:33:32 -0700

    Add another div that moves. Initial experiments with making divs move based on scroll event. Add a lorempixel background, which makes it easier to see the movement of the divs.

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 function tellMeTheX() {
   var x = $(window).scrollLeft()
-  console.log('Window"s x position:', x)
+  // console.log('Window"s x position:', x)
   return x
 }
 
-$(window).scroll(function(e) {
-  tellMeTheX()
-})
 
 // Move the block when any key is pressed
-addEventListener('keydown', function(e) {
-  var block = document.getElementsByClassName('move-me')
-  $(block[0]).css('left', '30px')
-  console.log('keydown', e.keyCode)
+// addEventListener('keydown', function(e) {
+//   var block = document.getElementsByClassName('move-me')
+//   $(block[0]).css('left', '30px')
+//   console.log('keydown', e.keyCode)
+// })
+
+// $(window).scroll(function(e) {
+//   tellMeTheX()
+// })
+
+var x1 = 200
+var x2 = 200
+
+$('.move-me').css('left', x1 + 'px')
+$('.move-me-2').css('left', x2 + 'px')
+
+addEventListener('scroll', function(e) {
+  x1 = x1
+  $('.move-me').css('left', x1 + 'px')
+
+  x2 = x2 + tellMeTheX()/30
+  $('.move-me-2').css('left', x2 + 'px')
+
+  console.log('x1', x1)
 })
